b32c7a08d69b14eb9967fede6dca21bc2d8c6848
sainikhilgoud10 <sainikhilgoud1994@gmail.com>
2023-12-06 21:21:11 +0530

    Updated to 33 sdk version

--- PlanetsSpaceFacts/app/src/main/java/pixelblur/com/planetsspacefacts/MainActivity.java
+++ PlanetsSpaceFacts/app/src/main/java/pixelblur/com/planetsspacefacts/MainActivity.java
@@ -4,9 +4,9 @@ package pixelblur.com.planetsspacefacts;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
